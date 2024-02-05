ba3de8db8557bfba82e7ab22c181add5d788cc87
Logan Mzz <loganmzz@users.noreply.github.com>
2024-02-05 15:47:01 +0100

    00 - Initialisation

---
+++ bin/git_resync_branches.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+function main() {
+    local branches=(
+        '00-init           00 - Initialisation'
+        '01-blueprint      01 - Blueprint'
+        '02-impl-default   02 - impl Default'
+        '03-modules        03 - Modules'
+        '04-impl-debug     04 - impl Debug'
+        '05-attribute      05 - Attribut'
+        '06-errors         06 - Gestion des erreurs'
+    )
+
+    local push_args=()
+    local branch=''
+    local branch_name=''
+    local branch_message=''
+    local commit=''
+    i=0
+    while [[ $i < ${#branches[@]} ]]; do
+        branch="${branches[i]}"
+        branch_name="$(sed -Ee 's/^([^ ]+).*/\1/' <<<"${branch}")" &&
+        branch_message="$(sed -Ee 's/^[^ ]+ *//' <<<"${branch}")" &&
+        branch_message="${branch_message##*( )}" &&
+        commit="$(git log '--pretty=format:%H' --reverse --grep "${branch_message}" --fixed-strings --max-count '1' 99-final)" &&
+        true || return $?
+        [[ ! -z "${commit}" ]] || {
+            echo "No commit found for branch '${branch_name}' with message '${branch_message}'" >&2
+            return 1
+        }
+        printf '%40s  %-15s  %s\n' "${commit}"  "${branch_name}"  "${branch_message}"
+        push_args+=( "${commit}:refs/heads/${branch_name}" )
+        i=$((i + 1))
+    done
+    echo
+
+    echo git push origin --force-with-lease "${push_args[@]}"
+}
+
+main "$@"
