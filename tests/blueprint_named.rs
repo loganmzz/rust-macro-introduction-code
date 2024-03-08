9b12f36646a310105ef713fb927f51170475cc9e
Logan Mzz <loganmzz@users.noreply.github.com>
2024-03-08 21:55:17 +0100

    04 - Debug (code)

--- tests/blueprint_named.rs
+++ tests/blueprint_named.rs
@@ -16,6 +16,17 @@ impl ::std::default::Default for Named {
     }
 }
 
+impl ::std::fmt::Debug for Named {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f
+            .debug_struct("Named")
+            .field("string", &self.string)
+            .field("number", &self.number)
+            .field("boolean", &self.boolean)
+            .finish()
+    }
+}
+
 // Test
 mod tests {
     use super::*;
@@ -28,5 +39,19 @@ mod tests {
         assert_eq!(String::default(), default.string, "string");
         assert_eq!(usize::default(), default.number, "number");
         assert_eq!(bool::default(), default.boolean, "boolean");
+    }
+
+    #[test]
+    fn named_impl_debug() {
+        let debug =  Named {
+            string: "world".to_string(),
+            number: 42,
+            boolean: false,
+        };
+
+        assert_eq!(
+            "Named { string: \"world\", number: 42, boolean: false }",
+            format!("{:?}", debug),
+        );
     }
 }
--- tests/blueprint_tuple.rs
+++ tests/blueprint_tuple.rs
@@ -15,6 +15,16 @@ impl ::std::default::Default for Tuple {
         )
     }
 }
+impl ::std::fmt::Debug for Tuple {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f
+            .debug_tuple("Tuple")
+            .field(&self.0)
+            .field(&self.1)
+            .field(&self.2)
+            .finish()
+    }
+}
 
 // Test
 mod tests {
@@ -28,5 +38,19 @@ mod tests {
         assert_eq!(String::default(), default.0, "0");
         assert_eq!(usize::default(), default.1, "1");
         assert_eq!(bool::default(), default.2, "2");
+    }
+
+    #[test]
+    fn tuple_impl_debug() {
+        let debug = Tuple(
+            "world".to_string(),
+            42,
+            false,
+        );
+
+        assert_eq!(
+            "Tuple(\"world\", 42, false)",
+            format!("{:?}", debug),
+        );
     }
 }
