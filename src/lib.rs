849718b69ac5fb6721ed6200001f206a6cca41b4
Logan Mzz <loganmzz@users.noreply.github.com>
2024-03-08 21:38:25 +0100

    00 - Initialisation (code)

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+#[proc_macro_derive(Data,)]
+pub fn data_macro_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = syn::parse_macro_input!(input as syn::DeriveInput);
+
+    // Permet de débugger l'instance de syn::DeriveInput
+    #[cfg(feature = "debug")]
+    eprintln!("{:#?}", input);
+
+    let output = data_macro_derive_impl(input);
+
+    // Permet de débugger le code généré par la macro
+    #[cfg(feature = "debug")]
+    eprintln!("{}", output);
+
+    output.into()
+}
+
+/// Génère le flux de token de manière testable
+/// car indépendant de proc_macro !
+fn data_macro_derive_impl(input: syn::DeriveInput) -> proc_macro2::TokenStream {
+    quote::quote!()
+}
