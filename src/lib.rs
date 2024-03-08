311b654e451961fdb5bbd541d47f47b9d1cfceb0
Logan Mzz <loganmzz@users.noreply.github.com>
2024-03-08 21:52:17 +0100

    02 - impl Default (code)

--- src/lib.rs
+++ src/lib.rs
@@ -12,5 +12,57 @@ pub fn data_macro_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStr
 }
 
 fn data_macro_derive_impl(input: syn::DeriveInput) -> proc_macro2::TokenStream {
-    quote::quote!()
+    let ident = &input.ident;
+    let (fields, delimiter) = match input {
+        syn::DeriveInput {
+            data: syn::Data::Struct(
+                syn::DataStruct {
+                    fields: syn::Fields::Named(
+                        syn::FieldsNamed {
+                            named: ref fields,
+                             ..
+                        }
+                    ),
+                    ..
+                }
+            ),
+            ..
+        } => (fields.iter().collect(), proc_macro2::Delimiter::Brace,),
+        syn::DeriveInput {
+            data: syn::Data::Struct(
+                syn::DataStruct {
+                    fields: syn::Fields::Unnamed(
+                        syn::FieldsUnnamed {
+                            unnamed: ref fields,
+                            ..
+                        }
+                    ),
+                    ..
+                }
+            ),
+            ..
+        } => (fields.iter().collect(), proc_macro2::Delimiter::Parenthesis,),
+        _ => (vec![], proc_macro2::Delimiter::None,),
+    };
+    let default_fields: proc_macro2::TokenStream = fields
+        .iter()
+        .map(|field| {
+            let prefix = if let Some(ref ident) = field.ident {
+                quote::quote!(#ident:)
+            } else {
+                quote::quote!()
+            };
+            quote::quote! {
+                #prefix ::std::default::Default::default(),
+            }
+        })
+        .collect();
+    let field_group = proc_macro2::Group::new(delimiter, default_fields);
+    quote::quote! {
+        impl ::std::default::Default for #ident {
+            fn default() -> Self {
+                Self #field_group
+            }
+        }
+    }
 }
